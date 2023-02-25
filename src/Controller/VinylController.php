c9423e17d52720c5514bd105e8468b1de22b5bbd
abu <abubakersuliman@outlook.com>
2023-02-25 17:30:54 +0400

    track list

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -10,8 +10,18 @@ class VinylController extends AbstractController
     #[Route('/')]
     public function homepage(): Response
     {
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+
         return $this->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
+            'tracks' => $tracks,
         ]);
     }
 };
