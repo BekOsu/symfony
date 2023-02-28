359f3c7373886e26d01cf00cb9fb9e67fab10e67
abu <abubakersuliman@outlook.com>
2023-02-28 15:27:53 +0400

    add base file

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
+use function Symfony\Component\String\u;
 
 class VinylController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
         $tracks = [
@@ -24,5 +26,14 @@ class VinylController extends AbstractController
             'tracks' => $tracks,
         ]);
     }
-};
 
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(string $slug = null): Response
+    {
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return $this->render('vinyl/browse.html.twig', [
+            'genre' => $genre
+        ]);
+    }
+}
