92e537dd9f7842d695f26387b7df9961faa76e5e
abu <abubakersuliman@outlook.com>
2023-03-01 13:41:46 +0400

    add LoggerInterface service

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Controller;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
-    public function getSong(int $id): Response
+    public function getSong(int $id, LoggerInterface $logger): Response
     {
         // TODO query the database
+        $logger->info('Returning API response for song {song}', [
+            'song' => $id,
+        ]);
         $song = [
             'id' => $id,
             'name' => 'Waterfalls',
