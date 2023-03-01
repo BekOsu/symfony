06c17de857b056415fca458ed0ca4f386e352ece
abu <abubakersuliman@outlook.com>
2023-03-01 13:18:07 +0400

    api json response

---
+++ src/Controller/SongController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id}')]
+    public function getSong($id): Response
+    {
+        // TODO query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+        return $this->json($song);
+    }
+}
